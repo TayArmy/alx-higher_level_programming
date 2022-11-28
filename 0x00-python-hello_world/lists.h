abae77b57b23e24f121f03a4fe0547a2560a2f6b
TayArmy <alabenitemitayo@gmail.com>
2022-11-28 04:58:16 -0800

    hello

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -3,6 +3,13 @@
 
 #include <stdlib.h>
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ *
+ */
 
 typedef struct listint_s
 {
@@ -14,5 +21,6 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
+
 
 #endif /* LISTS_H */
