a8cc8343a9ce700030be2b676ebf3e77869d4b86
TayArmy <alabenitemitayo@gmail.com>
2022-12-07 07:28:01 -0800

    more data

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,9 +6,9 @@ def to_subtract(list_num):
     for n in list_num:
         if max_list > n:
             to_sub += n
-    
+
     return (max_list - to_sub)
-    
+
 
 def roman_to_int(roman_string):
     if not roman_string:
@@ -23,7 +23,7 @@ def roman_to_int(roman_string):
     num = 0
     last_rom = 0
     list_num = [0]
-    
+
     for ch in roman_string:
         for r_num in list_keys:
             if r_num == ch:
@@ -32,9 +32,9 @@ def roman_to_int(roman_string):
                     list_num = [rom_n.get(ch)]
                 else:
                     list_num.append(rom_n.get(ch))
-                
+
                 last_rom = rom_n.get(ch)
-    
+
     num += to_subtract(list_num)
-    
+
     return (num)
